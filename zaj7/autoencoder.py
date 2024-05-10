ca56a0dd53e0b15335085962991c75b14cbdb9a5
HOME-VM-TOSCHOOL <mat.dok123@gmail.com>
2024-05-10 15:31:46 +0200

    autoencoder

--- zaj7/autoencoder.py
+++ zaj7/autoencoder.py
@@ -1,16 +1,26 @@
+from enum import Enum
 from os import sep
 import random
 
 import numpy as np
 from scipy.special import expit
 
-RANDOM = True
-c = 0.8
-epsilon = 1e-5
-beta = 1
-max_iter = 1e6
-BATCH = 1
 
+class VisualEnum(Enum):
+    FALSE = 0
+    TRUE = 1
+    BOTH = 2
+
+
+RANDOM = True
+VISUAL = VisualEnum.BOTH
+c = 1.3
+epsilon = 1e-5
+beta = 0.5
+max_iter = 1e6
+BATCH = 2
+
+ZAD = 2
 
 def sigmoid(z):
     return expit(beta * z)
@@ -33,8 +43,8 @@ def gradient_outer(
 ) -> tuple[np.ndarray, np.ndarray]:
     err_sig = error_prime * sigmoid_prime_value
     return (
-        np.multiply(err_sig, values.tolist() + [1]),
-        np.multiply(err_sig.reshape(-1, 1), weights[:, :-1]),
+        np.multiply(err_sig.reshape(-1, 1), [values.tolist() + [1]] * weights.shape[0]),
+        np.sum(np.multiply(err_sig.reshape(-1, 1), weights[:, :-1]), axis=0),
     )
 
 
@@ -80,10 +90,36 @@ def propagate(
 
 def show_array(list, len_vertical, len_horizontal):
     for i in range(len_vertical):
+        to_show = ""
         for j in range(len_horizontal):
-            print(np.round(list[i * len_horizontal + j], 3), end="\t")
+            match VISUAL:
+                case VisualEnum.FALSE:
+                    print("x" if list[i * len_horizontal + j] > 0.5 else "o", end="\t")
+                case VisualEnum.TRUE:
+                    print(np.round(list[i * len_horizontal + j], 4), end="\t")
+                case VisualEnum.BOTH:
+                    print(np.round(list[i * len_horizontal + j], 4), end="\t")
+                    to_show += (
+                        f"{'x' if list[i * len_horizontal + j] > 0.5 else 'o'}" + "\t"
+                    )
+        print(to_show)
         print()
     print()
+
+
+def show_tests():
+    for test in input:
+        layer_two_sigmoid, _, layer_one_sigmoid, _ = propagate(
+            test, weights_inner, weights_outer
+        )
+        print("Input:")
+        show_array(test, 3, 3)
+
+        print("Inner layer:")
+        show_array(layer_one_sigmoid, 1, 2)
+
+        print("Output layer:")
+        show_array(layer_two_sigmoid, 3, 3)
 
 
 input_one = np.array(
@@ -105,39 +141,121 @@ input = np.array([input_one.flatten(), input_two.flatten()])
 inner_node_number = 2
 expected = input.copy()
 
-weights_inner_array = [
-    [0 for _ in range(len(input[0]) + 1)] for _ in range(inner_node_number)
-]
-weights_inner_array[0][0] = 20
-weights_inner_array[0][9] = -10
-weights_inner_array[1][2] = 20
-weights_inner_array[1][9] = -10
+if ZAD == 1:
+    weights_inner_array = [
+        [0 for _ in range(len(input[0]) + 1)] for _ in range(inner_node_number)
+    ]
+    weights_inner_array[0][0] = 20
+    weights_inner_array[0][9] = -10
+    weights_inner_array[1][2] = 20
+    weights_inner_array[1][9] = -10
 
-weights_outer_array = [
-    [20 * input[i][j] if i in [0, 1] else -10 for i in range(inner_node_number + 1)]
-    for j in range(len(expected[0]))
-]
+    weights_outer_array = [
+        [20 * input[i][j] if i in [0, 1] else -10 for i in range(inner_node_number + 1)]
+        for j in range(len(expected[0]))
+    ]
 
+    weights_inner = np.array(weights_inner_array)
+    weights_outer = np.array(weights_outer_array)
 
-weights_inner = np.array(weights_inner_array)
-weights_outer = np.array(weights_outer_array)
-
-# first iteration for first input
-layer_two_sigmoid, layer_two, layer_one_sigmoid, layer_one = propagate(
-    input[0], weights_inner, weights_outer
-)
-print("First input:\nInner layer:")
-show_array(layer_one_sigmoid, 1, 2)
-print("Output layer:")
-show_array(layer_two_sigmoid, 3, 3)
+    show_tests()
 
 
-# first iteration for second input
-layer_two_sigmoid, layer_two, layer_one_sigmoid, layer_one = propagate(
-    input[1], weights_inner, weights_outer
-)
+if ZAD == 2:
+    weights_inner_array = [
+        [0 for _ in range(len(input[0]) + 1)] for _ in range(inner_node_number)
+    ]
+    weights_inner_array[0][0] = 20
+    weights_inner_array[0][9] = -10
+    weights_inner_array[1][2] = 20
+    weights_inner_array[1][9] = -10
 
-print("Second input:\nInner layer:")
-show_array(layer_one_sigmoid, 1, 2)
-print("Output layer:")
-show_array(layer_two_sigmoid, 3, 3)
+    weights_outer_array = [
+        [20 * input[i][j] if i in [0, 1] else -10 for i in range(inner_node_number + 1)]
+        for j in range(len(expected[0]))
+    ]
+
+    if RANDOM:
+        weights_inner_array = [
+            [random.uniform(-10, 10) for _ in range(len(input[0]) + 1)]
+            for _ in range(inner_node_number)
+        ]
+
+        weights_outer_array = [
+            [random.uniform(-10, 10) for _ in range(inner_node_number + 1)]
+            for _ in range(len(expected[0]))
+        ]
+
+    weights_inner = np.array(weights_inner_array)
+    weights_outer = np.array(weights_outer_array)
+
+    # first iteration
+    layer_two_sigmoid, layer_two, layer_one_sigmoid, layer_one = propagate(
+        input[0], weights_inner, weights_outer
+    )
+    err_prime_diff = error_prime(layer_two_sigmoid, expected[0])
+
+    gradient_outer_value, cashed_partial_value = gradient_outer(
+        err_prime_diff,
+        weights_outer,
+        layer_one_sigmoid,
+        sigmoid_prime(layer_two),
+    )
+    weights_outer_new = weights_outer - c * gradient_outer_value
+    gradient_inner_value = gradient_inner(
+        cashed_partial_value,
+        sigmoid_prime(layer_one),
+        input[0],
+    )
+    weights_inner_new = weights_inner - c * gradient_inner_value
+    iter = 0
+    loop_con_val = loop_condition_value(
+        weights_inner,
+        weights_outer,
+        weights_inner_new,
+        weights_outer_new,
+    )
+
+    while loop_con_val > epsilon and iter < max_iter:
+        gradient_outer_value_batch = np.zeros_like(gradient_outer_value)
+        gradient_inner_value_batch = np.zeros_like(gradient_inner_value)
+        for i in range(BATCH):
+            iter += 1
+
+            weights_inner = weights_inner_new
+            weights_outer = weights_outer_new
+
+            layer_two_sigmoid, layer_two, layer_one_sigmoid, layer_one = propagate(
+                input[iter % len(input)], weights_inner, weights_outer
+            )
+            err_prime_diff = error_prime(layer_two_sigmoid, expected[iter % len(input)])
+
+            gradient_outer_value, cashed_partial_value = gradient_outer(
+                err_prime_diff,
+                weights_outer,
+                layer_one_sigmoid,
+                sigmoid_prime(layer_two),
+            )
+            gradient_inner_value = gradient_inner(
+                cashed_partial_value,
+                sigmoid_prime(layer_one),
+                input[iter % len(input)],
+            )
+
+            gradient_outer_value_batch += gradient_outer_value
+            gradient_inner_value_batch += gradient_inner_value
+
+        weights_outer_new = weights_outer - c * gradient_outer_value_batch
+
+        weights_inner_new = weights_inner - c * gradient_inner_value_batch
+        loop_con_val = loop_condition_value(
+            weights_inner,
+            weights_outer,
+            weights_inner_new,
+            weights_outer_new,
+        )
+
+        if iter % 10000 == 0:
+            print(f"iter: {iter}, loop_con_val: {round(loop_con_val, 8)}")
+
+    show_tests()
