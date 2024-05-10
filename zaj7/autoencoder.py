f9dc8135e7e7fa6f75820851defd3bc6eff4f2eb
HOME-VM-TOSCHOOL <mat.dok123@gmail.com>
2024-05-10 12:35:26 +0200

    encoder zad1

--- zaj7/autoencoder.py
+++ zaj7/autoencoder.py
@@ -1,12 +1,13 @@
+from os import sep
 import random
 
 import numpy as np
 from scipy.special import expit
 
 RANDOM = True
-c = 2
+c = 0.8
 epsilon = 1e-5
-beta = 2
+beta = 1
 max_iter = 1e6
 BATCH = 1
 
@@ -75,3 +76,68 @@ def propagate(
     layer_two_sigmoid = sigmoid(layer_two)
 
     return (layer_two_sigmoid, layer_two, layer_one_sigmoid, layer_one)
+
+
+def show_array(list, len_vertical, len_horizontal):
+    for i in range(len_vertical):
+        for j in range(len_horizontal):
+            print(np.round(list[i * len_horizontal + j], 3), end="\t")
+        print()
+    print()
+
+
+input_one = np.array(
+    [
+        [1, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0],
+    ]
+)
+input_two = np.array(
+    [
+        [0, 0, 1],
+        [0, 0, 1],
+        [0, 0, 1],
+    ]
+)
+
+input = np.array([input_one.flatten(), input_two.flatten()])
+inner_node_number = 2
+expected = input.copy()
+
+weights_inner_array = [
+    [0 for _ in range(len(input[0]) + 1)] for _ in range(inner_node_number)
+]
+weights_inner_array[0][0] = 20
+weights_inner_array[0][9] = -10
+weights_inner_array[1][2] = 20
+weights_inner_array[1][9] = -10
+
+weights_outer_array = [
+    [20 * input[i][j] if i in [0, 1] else -10 for i in range(inner_node_number + 1)]
+    for j in range(len(expected[0]))
+]
+
+
+weights_inner = np.array(weights_inner_array)
+weights_outer = np.array(weights_outer_array)
+
+# first iteration for first input
+layer_two_sigmoid, layer_two, layer_one_sigmoid, layer_one = propagate(
+    input[0], weights_inner, weights_outer
+)
+print("First input:\nInner layer:")
+show_array(layer_one_sigmoid, 1, 2)
+print("Output layer:")
+show_array(layer_two_sigmoid, 3, 3)
+
+
+# first iteration for second input
+layer_two_sigmoid, layer_two, layer_one_sigmoid, layer_one = propagate(
+    input[1], weights_inner, weights_outer
+)
+
+print("Second input:\nInner layer:")
+show_array(layer_one_sigmoid, 1, 2)
+print("Output layer:")
+show_array(layer_two_sigmoid, 3, 3)
